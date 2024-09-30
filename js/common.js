9bea8ca84e689bf8886b8c61d2ba5a94f8222bcb
universe119 <ruddh119@gmail.com>
2024-09-30 09:16:46 +0900

    anime연결 및 스크롤 동작 테스트 완료

--- js/common.js
+++ js/common.js
@@ -8,6 +8,6 @@ btnMo.addEventListener("click", () => {
 //브라우저 리사이즈시 브라우저폭의 너비가 1000px이상일때 강제로 mobilePanel의 클래스의 on을 제거해서 숨김처리
 window.addEventListener("resize", () => {
 	const wid = window.innerWidth;
-	console.log(wid);
+	// console.log(wid);
 	if (wid >= 1000) mobilePanel.classList.remove("on");
 });
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 import Anime from "./anime.js";
+
+const h1 = document.querySelector("header h1");
+
+h1.addEventListener("click", e => {
+	e.preventDefault();
+
+	new Anime(window, { scroll: 700 }, { duration: 1000 });
+});
