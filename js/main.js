e147b264dbc91c6c068880e4c1a79e940af0241e
universe119 <ruddh119@gmail.com>
2024-09-30 17:01:40 +0900

    브라우저 리사이즈시 현재 활성화된 버튼 순번으로 스크롤 위치값을 바로바로 갱신처리

--- js/main.js
+++ js/main.js
@@ -9,25 +9,24 @@ const scroll_btns = document.querySelectorAll(".scroller li");
 //init (get section position array)
 //처음 로딩시 호출해서 초기 배열값 담아줌
 getPosArr(secArr);
-console.log(posArr);
 
 //get  position array whenever scrolls
 //브라우저 리사이즈 이벤트 발생할 때마다 새로운 값으로 갱신
 window.addEventListener("resize", () => {
 	getPosArr(secArr);
-	console.log(posArr);
+	modifyPos();
 });
 
 //scroll btn event
 scroll_btns.forEach((btn, idx) => {
 	btn.addEventListener("click", () => {
-		new Anime(window, { scroll: posArr[idx] });
-
-		//btn activation
-		// scroll_btns.forEach(el => el.classList.remove("on"));
-		// scroll_btns[idx].classList.add("on");
-		activation(scroll_btns, idx);
+		moveScroll(idx);
 	});
+
+	//btn activation
+	// scroll_btns.forEach(el => el.classList.remove("on"));
+	// scroll_btns[idx].classList.add("on");
+	// activation(scroll_btns, idx);
 });
 
 // window scroll event bind
@@ -65,4 +64,21 @@ function getPosArr(arrEl) {
 	posArr = [];
 	// 위 초기화 변수를 넣지 않으면 push는 값을 계속 추가 한다.
 	for (let el of arrEl) posArr.push(el.offsetTop);
+}
+
+//move scroll
+function moveScroll(index, speed = 500) {
+	new Anime(window, { scroll: posArr[index] }, { duration: speed });
+}
+
+//when resize modifying scroll position
+//현재 활성화된 버튼의 순번 위치로 모션없이 바로 스크롤 이동처리
+function modifyPos() {
+	const activeEl = document.querySelector("li.on");
+	//Array.from(유사배열) - 유사배열을 순수배열 형태로 변환해서 반환
+	//전체배열.indexOf(특정배열) : 전체배열에서 특정배열값의 순번을 반환
+	//scroll_btns라는 버튼 그룹에서 on클래스가 붙어있는 버튼의 순서값을 반환
+	const activeIndex = Array.from(scroll_btns).indexOf(activeEl);
+	moveScroll(activeIndex, 0);
+	console.log(activeIndex);
 }
